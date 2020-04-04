94d6cba7a4438625ff75e8c27b8ad59843de66ff
leynier <leynier41@gmail.com>
2020-04-03 21:37:10 -0400

    Implement check of json

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from json import dump, load
 from os import makedirs
+from jsonschema import validate
 
 
-def main():
+def check():
+    data = load(open('data/covid19-cuba.json', encoding="utf-8"))
+    schema = load(open('schema.json', encoding="utf-8"))
+    validate(data, schema)
+    return True
+
+
+def mimificate():
     makedirs('data/mini', exist_ok=True)
-    data = load(open('data/covid19-cuba.json'))
-    dump(data, open('data/mini/covid19-cuba.json', 'w'))
+    data = load(open('data/covid19-cuba.json', encoding='utf-8'))
+    dump(data, open('data/mini/covid19-cuba.json', mode='w', encoding='utf-8'))
 
 
 if __name__ == "__main__":
-    main()
+    if check():
+        mimificate()

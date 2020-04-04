593d71cc8580e015ff8528f2238b9e6cf9f15eda
leynier <leynier41@gmail.com>
2020-04-04 05:58:07 -0400

    Update main

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from json import dump, load
-from os import makedirs
-from jsonschema import validate
-
-
-def check():
-    data = load(open('data/covid19-cuba.json', encoding="utf-8"))
-    schema = load(open('schema.json', encoding="utf-8"))
-    validate(data, schema)
-    return True
-
-
-def mimificate():
-    makedirs('data/mini', exist_ok=True)
-    data = load(open('data/covid19-cuba.json', encoding='utf-8'))
-    dump(data, open('data/mini/covid19-cuba.json', mode='w', encoding='utf-8'))
-
+from app import run
 
 if __name__ == "__main__":
-    if check():
-        mimificate()
+    run()

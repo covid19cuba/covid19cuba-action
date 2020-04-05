216213b0c14fc5995a0e9441e4223e58dfd439e2
leynier <leynier41@gmail.com>
2020-04-05 01:11:40 -0400

    Add province codes

---
+++ app/v1/province_codes.py
@@ -0,0 +1,19 @@
+province_codes = {
+    '00': 'Desconocida',
+    '23': 'La Habana',
+    '25': 'Matanzas',
+    '27': 'Cienfuegos',
+    '28': 'Sancti Spíritus',
+    '31': 'Las Tunas',
+    '32': 'Holguín',
+    '33': 'Granma',
+    '34': 'Santiago de Cuba',
+    '40.01': 'Isla de la Juventud',
+    '30': 'Camagüey',
+    '29': 'Ciego de Ávila',
+    '26': 'Villa Clara',
+    '35': 'Guantánamo',
+    '21': 'Pinar del Río',
+    '22': 'Artemisa',
+    '24': 'Mayabeque'
+}

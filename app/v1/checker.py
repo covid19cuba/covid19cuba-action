8a8c523498f358f960e85ebf3581a0d76282bfa4
leynier <leynier41@gmail.com>
2020-04-05 00:07:30 -0400

    Change 'schema.json' by 'schema.py'

--- app/v1/checker.py
+++ app/v1/checker.py
@@ -1,9 +1,9 @@
 from json import dump, load
 from jsonschema import validate
+from .schema import schema
 
 
 def check():
     data = load(open('data/covid19-cuba.json', encoding='utf-8'))
-    schema = load(open('app/v1/schema.json', encoding='utf-8'))
     validate(data, schema)
     return True

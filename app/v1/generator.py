574381ea19da9bf63fd530912cd04ff0da2ee810
cbermudez <cbermu.97@gmail.com>
2020-04-05 15:50:37 -0400

    refs #12 - Implement comparison_of_accumulated_cases method

--- app/v1/generator.py
+++ app/v1/generator.py
@@ -328,4 +328,16 @@ def affected_municipalities(data):
 
 
 def comparison_of_accumulated_cases(data):
-    pass
+    world = data['data_world']
+    days = list(data['data_cuba']['casos']['dias'].values())
+    diagnosed = [x['diagnosticados'] for x in days if 'diagnosticados' in x]
+    accum_cuba = [0]
+    for cases in diagnosed:
+        accum = accum_cuba[len(accum_cuba) - 1] + len(cases) 
+        accum_cuba.append(accum)
+    accum_cuba = accum_cuba[:-1]
+    try:
+        world['Cuba'] = accum_cuba
+    except KeyError:
+        pass
+    return world

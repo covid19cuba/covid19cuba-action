313882d3584141b65a856c183489622c3a781023
leynier <leynier41@gmail.com>
2020-04-05 01:22:18 -0400

    Implement app version code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
+from os import makedirs
 from .v1 import run as v1run
+
+APP_VERSION_CODE = 6
 
 
 def run(debug=False):
+    main_run()
     v1run(debug)
+
+
+def main_run():
+    makedirs('api', exist_ok=True)
+    with open('api/app_version_code', mode='w') as file:
+        file.write(str(APP_VERSION_CODE))

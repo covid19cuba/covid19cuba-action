8098b764e8c0819457fa81a88cbebeb76f3cc98d
leynier <leynier41@gmail.com>
2020-04-09 10:36:02 -0400

    Improve report of errors

--- app/v1/checker.py
+++ app/v1/checker.py
@@ -1,5 +1,5 @@
 from json import dump, load
-from jsonschema import validate
+from jsonschema import Draft7Validator
 from .schema import schema
 from .municipality_codes import municipality_codes
 from .province_codes import province_codes
@@ -7,8 +7,42 @@ from .province_codes import province_codes
 
 def check():
     data = load(open('data/covid19-cuba.json', encoding='utf-8'))
-    validate(data, schema)
-    check_provinces_and_municipalities(data)
+    validator = Draft7Validator(schema)
+    index = 1
+    for error in sorted(validator.iter_errors(data), key=str):
+        location = error.path
+        try:
+            keys = []
+            loc = 0
+            for i, value in enumerate(list(error.path)):
+                keys.append(value)
+                if value == 'diagnosticados':
+                    loc = i + 1
+                    break
+            if loc < len(list(error.path)):
+                loc = list(error.path)[loc]
+            value = data
+            for key in keys:
+                value = value[key]
+            location = f'Id: {value[loc]["id"]}'
+        except:
+            pass
+        end = f', {error.path}' if location != error.path else ''
+        print('============================================')
+        print(f'Error {index}:')
+        print(f'Cause: {error.message}')
+        print(f'Location: {location}{end}')
+        print('============================================')
+        index += 1
+    for message, path in check_provinces_and_municipalities(data):
+        print('============================================')
+        print(f'Error {index}:')
+        print(f'Cause: {message}')
+        print(f'Location: {path}')
+        print('============================================')
+        index += 1
+    if index > 1:
+        raise Exception('Invalid JSON.')
     return True
 
 
@@ -19,10 +53,10 @@ def check_provinces_and_municipalities(data):
         actual = value['provincia']
         expected = province_codes.get(code)
         if actual != expected:
-            message = f'Expected: {expected}, Found: {actual}.'
-            message = f'Invalid "provincia" by "dpacode_provincia". {message}'
-            message = f'{message} Id: {key}'
-            raise Exception(message)
+            message = f'Invalid "provincia" by "dpacode_provincia". ' + \
+                f'Expected: {expected}, Found: {actual}.'
+            path = f'Id: {key}, {["centros_aislamiento", key]}'
+            yield message, path
     for key in data['centros_diagnostico']:
         value = data['centros_diagnostico'][key]
         code = value['dpacode_provincia']
@@ -30,29 +64,31 @@ def check_provinces_and_municipalities(data):
         expected = province_codes.get(code)
         if actual != expected:
             message = f'Invalid "provincia" by "dpacode_provincia". ' + \
-                f'Expected: {expected}, Found: {actual}. ' + \
-                f'Id: {key}'
-            raise Exception(message)
-    days = list(data['casos']['dias'].values())
-    for diagnosed in (x['diagnosticados'] for x in days if 'diagnosticados' in x):
-        for value in diagnosed:
-            code = value['dpacode_provincia_deteccion']
-            actual = value['provincia_detección']
-            expected = province_codes.get(code)
-            if actual != expected and \
-                    (actual or expected != 'Desconocida'):
-                message = f'Invalid "provincia_detección" by' + \
-                    f'"dpacode_provincia_deteccion". ' + \
-                    f'Expected: {expected}, Found: {actual}. ' + \
-                    f'Id: {value["id"]}'
-                raise Exception(message)
-            code = value['dpacode_municipio_deteccion']
-            actual = value['municipio_detección']
-            expected = municipality_codes.get(code)
-            if actual != expected and \
-                    (actual or expected != 'Desconocido'):
-                message = f'Invalid "municipio_detección" by ' + \
-                    f'"dpacode_municipio_deteccion". ' + \
-                    f'Expected: {expected}, Found: {actual}. ' + \
-                    f'Id: {value["id"]}'
-                raise Exception(message)
+                f'Expected: {expected}, Found: {actual}.'
+            path = f'Id: {key}, {["centros_diagnostico", key]}'
+            yield message, path
+    for day in data['casos']['dias']:
+        x = data['casos']['dias'][day]
+        if 'diagnosticados' in x:
+            diagnosed = x['diagnosticados']
+            for i, value in enumerate(diagnosed):
+                code = value['dpacode_provincia_deteccion']
+                actual = value['provincia_detección']
+                expected = province_codes.get(code)
+                if actual != expected and \
+                        (actual or expected != 'Desconocida'):
+                    message = f'Invalid "provincia_detección" by' + \
+                        f'"dpacode_provincia_deteccion". ' + \
+                        f'Expected: {expected}, Found: {actual}.'
+                    path = f'Id: {value["id"]}, {["casos", "dias", day, "diagnosticados", i, value["id"]]}'
+                    yield message, path
+                code = value['dpacode_municipio_deteccion']
+                actual = value['municipio_detección']
+                expected = municipality_codes.get(code)
+                if actual != expected and \
+                        (actual or expected != 'Desconocido'):
+                    message = f'Invalid "municipio_detección" by ' + \
+                        f'"dpacode_municipio_deteccion". ' + \
+                        f'Expected: {expected}, Found: {actual}.'
+                    path = f'Id: {value["id"]}, {["casos", "dias", day, "diagnosticados", i, value["id"]]}'
+                    yield message, path

45831d0a82acdd2507b8244e0b2cca8273bc2647
leynier <leynier41@gmail.com>
2020-04-09 11:06:51 -0400

    Implement bot for report to group

---
+++ app/send_message.py
@@ -0,0 +1,8 @@
+import telegram
+
+bot_token = '1122724836:AAF6K1XjbGfVykqoqUTARQA0Qa7A2ItEnZ8'
+group_id = -1001328230896
+
+def send(msg, chat_id=group_id, token=bot_token):
+	bot = telegram.Bot(token=token)
+	bot.sendMessage(chat_id=chat_id, text=msg)

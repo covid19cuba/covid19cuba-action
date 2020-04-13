ba235d4f3ed0776b047b2f33681ed62643286f10
cbermudez <cbermu.97@gmail.com>
2020-04-13 17:30:38 -0400

    Change changelog json format

--- app/v1/changelog.py
+++ app/v1/changelog.py
@@ -1,113 +1,115 @@
-changelog = [
-    {
-        "version": "0.1.1",
-        "date": "2020/03/29",
-        "changed": [
-            "Se cambió el idioma del nombre de los países a comparar con Cuba al español.",
-            "Se actualizó el enlace de Matcom.",
-        ],
-        "added": [
+changelog = { 
+    "changelog": [
+        {
+            "version": "0.1.1",
+            "date": "2020/03/29",
+            "changed": [
+                "Se cambió el idioma del nombre de los países a comparar con Cuba al español.",
+                "Se actualizó el enlace de Matcom.",
+            ],
+            "added": [
 
-        ],
-        "fixed": [
-            "Se corrigieron errores ortográficos.",
-            "Se corrigieron los errores en las leyendas de las gráficas.",
-        ],
-        "deleted": [
+            ],
+            "fixed": [
+                "Se corrigieron errores ortográficos.",
+                "Se corrigieron los errores en las leyendas de las gráficas.",
+            ],
+            "deleted": [
 
-        ],
-    },
-    {
-        "version": "0.2.0",
-        "date": "2020/03/30",
-        "changed": [
-            "Se mejoraron varias gráficas existentes."
-        ],
-        "added": [
-            "Se añadió una presentación con consejos útiles sobre el Covid-19.",
-            "Se añadió una gráfica con información sobre la relación de los Test aplicados.",
-            "Se añadió una gráfica con información sobre la evolución de los Test aplicados.",
-            "Se añadieron varios enlaces de interés al menú lateral."
-        ],
-        "fixed": [
-            "Se arreglaron pequeños errores existentes en la aplicación."
-        ],
-        "deleted": [
+            ],
+        },
+        {
+            "version": "0.2.0",
+            "date": "2020/03/30",
+            "changed": [
+                "Se mejoraron varias gráficas existentes."
+            ],
+            "added": [
+                "Se añadió una presentación con consejos útiles sobre el Covid-19.",
+                "Se añadió una gráfica con información sobre la relación de los Test aplicados.",
+                "Se añadió una gráfica con información sobre la evolución de los Test aplicados.",
+                "Se añadieron varios enlaces de interés al menú lateral."
+            ],
+            "fixed": [
+                "Se arreglaron pequeños errores existentes en la aplicación."
+            ],
+            "deleted": [
 
-        ],
-    },
-    {
-        "version": "0.2.1",
-        "date": "2020/03/31",
-        "changed": [
-            "Se realizaron mejoras a los mapas.",
-        ],
-        "added": [
-            "Se añadió un botón para actualizar la información en la esquina superior derecha.",
-            "Se añadió un enlace al bot de Telegram de Covid-19 Cuba Data en el menú lateral.",
-        ],
-        "fixed": [
+            ],
+        },
+        {
+            "version": "0.2.1",
+            "date": "2020/03/31",
+            "changed": [
+                "Se realizaron mejoras a los mapas.",
+            ],
+            "added": [
+                "Se añadió un botón para actualizar la información en la esquina superior derecha.",
+                "Se añadió un enlace al bot de Telegram de Covid-19 Cuba Data en el menú lateral.",
+            ],
+            "fixed": [
 
-        ],
-        "deleted": [
+            ],
+            "deleted": [
 
-        ],
-    },
-    {
-        "version": "0.3.0",
-        "date": "2020/04/02",
-        "changed": [
-            "Se modificó el gráfico de Cantidad de Test aplicados acumulados por días a Cantidad de Test aplicados por días.",
-        ],
-        "added": [
-            "Se añadió el listado de las 10 provincias y 10 municipios con más casos diagnosticados.",
-            "Se añadió una notificación para recordar el aplauso de las 9 de la noche dedicado quienes trabajan por la salud y seguridad del pueblo.",
-        ],
-        "fixed": [
-            "Se arreglaron errores en los mapas, como los colores de la escala.",
-            "Se solucionaron errores menores en la aplicación.",
+            ],
+        },
+        {
+            "version": "0.3.0",
+            "date": "2020/04/02",
+            "changed": [
+                "Se modificó el gráfico de Cantidad de Test aplicados acumulados por días a Cantidad de Test aplicados por días.",
+            ],
+            "added": [
+                "Se añadió el listado de las 10 provincias y 10 municipios con más casos diagnosticados.",
+                "Se añadió una notificación para recordar el aplauso de las 9 de la noche dedicado quienes trabajan por la salud y seguridad del pueblo.",
+            ],
+            "fixed": [
+                "Se arreglaron errores en los mapas, como los colores de la escala.",
+                "Se solucionaron errores menores en la aplicación.",
 
-        ],
-        "deleted": [
+            ],
+            "deleted": [
 
-        ],
-    },
-    {
-        "version": "0.3.1",
-        "date": "2020/04/04",
-        "changed": [
-            "Se cambió el país por defecto para comparar con Cuba.",
-        ],
-        "added": [
+            ],
+        },
+        {
+            "version": "0.3.1",
+            "date": "2020/04/04",
+            "changed": [
+                "Se cambió el país por defecto para comparar con Cuba.",
+            ],
+            "added": [
 
-        ],
-        "fixed": [
-            "Se arreglaron errores en las tablas de los 10 provincias y 10 municipios con mayor cantidad de casos diagnosticados.",
-        ],
-        "deleted": [
+            ],
+            "fixed": [
+                "Se arreglaron errores en las tablas de los 10 provincias y 10 municipios con mayor cantidad de casos diagnosticados.",
+            ],
+            "deleted": [
 
-        ],
-    },
-    {
-        "version": "0.4.0",
-        "date": "2020/04/08",
-        "changed": [
-            "Se cambió el comportamiento del país por defecto con que se compara Cuba. Ahora este será el último país con que se seleccionó para comparar.",
-            "Se cambiaron los datos en algunas gráficas.",
-        ],
-        "added": [
-            "Se añadió la información de los casos activos a la gráfica de Evolución de Casos por Días.",
-            "Se añadió una gráfica de la Evolución de Altas por Días.",
-            "Se añadió una gráfica de la Evolución de Muertes por Días.",
-            "Se añadió una tabla con los 20 países con más casos acumulados.",
-            "Se añadió una página de configuración accesible desde el menú lateral.",
-            "Se añadió la posibilidad de escoger el modo de conexión entre: solo utilizar un servidor ubicado en Cuba, utilizar solo uno externo a Cuba o por último utilizar primero uno ubicado en Cuba y, si algo falla durante el proceso, utilizar el externo a Cuba.",
-        ],
-        "fixed": [
-            "Se solucionó el problema que impedía que las tablas de los municipios y provincias con más casos se mostrasen.",
-        ],
-        "deleted": [
+            ],
+        },
+        {
+            "version": "0.4.0",
+            "date": "2020/04/08",
+            "changed": [
+                "Se cambió el comportamiento del país por defecto con que se compara Cuba. Ahora este será el último país con que se seleccionó para comparar.",
+                "Se cambiaron los datos en algunas gráficas.",
+            ],
+            "added": [
+                "Se añadió la información de los casos activos a la gráfica de Evolución de Casos por Días.",
+                "Se añadió una gráfica de la Evolución de Altas por Días.",
+                "Se añadió una gráfica de la Evolución de Muertes por Días.",
+                "Se añadió una tabla con los 20 países con más casos acumulados.",
+                "Se añadió una página de configuración accesible desde el menú lateral.",
+                "Se añadió la posibilidad de escoger el modo de conexión entre: solo utilizar un servidor ubicado en Cuba, utilizar solo uno externo a Cuba o por último utilizar primero uno ubicado en Cuba y, si algo falla durante el proceso, utilizar el externo a Cuba.",
+            ],
+            "fixed": [
+                "Se solucionó el problema que impedía que las tablas de los municipios y provincias con más casos se mostrasen.",
+            ],
+            "deleted": [
 
-        ],
-    },
-]
+            ],
+        },
+    ]
+}

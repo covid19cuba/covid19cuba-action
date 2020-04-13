7d2ddcf6a49f8aa6f1d19b297aee496c8a4c450c
leynier <leynier41@gmail.com>
2020-04-13 00:44:07 -0400

    Implement version state

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,7 +1,11 @@
+from hashlib import sha1
 from .checker import check
 from .generator import generate
 from .generator_provinces import generate as generate_provinces
+from .utils import dump_util
 from ..send_message import send
+
+APP_VERSION_CODE = 6
 
 
 def run(debug=False):
@@ -9,9 +13,26 @@ def run(debug=False):
         check()
         generate(debug)
         generate_provinces(debug)
+        build_state(debug)    
         send_msg('GitHub Action run successfully.', debug)
     except Exception as e:
         send_msg(e, debug)
+
+
+def build_state(debug):
+    dump_util('api/v1', state, debug=debug)
+
+
+def state(data):
+    result = {
+        'version': APP_VERSION_CODE,
+        'cache': None
+    }
+    with open('api/v1/all.json', encoding='utf-8') as file:
+        text = file.read()
+        cache = sha1(text.encode())
+        result['cache'] = cache.hexdigest()
+    return result
 
 
 def send_msg(message, debug):

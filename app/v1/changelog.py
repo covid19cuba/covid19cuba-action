20288a248f639958a811b24d37a033267d7dbea1
leynier <leynier41@gmail.com>
2020-04-23 17:51:58 -0400

    Update changelog

--- app/v1/changelog.py
+++ app/v1/changelog.py
@@ -1,136 +1,168 @@
 changelog = { 
-    "changelog": [
+    'changelog': [
         {
-            "version": "0.1.1",
-            "date": "2020/03/29",
-            "changed": [
-                "Idioma del nombre de los países a comparar con Cuba al español.",
-                "Enlace de Matcom.",
+            'version': '0.1.1',
+            'date': '2020/03/29',
+            'changed': [
+                'Idioma del nombre de los países a comparar con Cuba al español.',
+                'Enlace de Matcom.',
             ],
-            "added": [
+            'added': [
 
             ],
-            "fixed": [
-                "Errores ortográficos.",
-                "Errores en las leyendas de las gráficas.",
+            'fixed': [
+                'Errores ortográficos.',
+                'Errores en las leyendas de las gráficas.',
             ],
-            "deleted": [
+            'deleted': [
 
             ],
         },
         {
-            "version": "0.2.0",
-            "date": "2020/03/30",
-            "changed": [
-                "Varias gráficas existentes."
+            'version': '0.2.0',
+            'date': '2020/03/30',
+            'changed': [
+                'Varias gráficas existentes.'
             ],
-            "added": [
-                "Presentación con consejos útiles sobre el Covid-19.",
-                "Gráfica con información sobre la relación de los Test aplicados.",
-                "Gráfica con información sobre la evolución de los Test aplicados.",
-                "Varios enlaces de interés al menú lateral."
+            'added': [
+                'Presentación con consejos útiles sobre el Covid-19.',
+                'Gráfica con información sobre la relación de los Test aplicados.',
+                'Gráfica con información sobre la evolución de los Test aplicados.',
+                'Varios enlaces de interés al menú lateral.'
             ],
-            "fixed": [
-                "Pequeños errores existentes en la aplicación."
+            'fixed': [
+                'Pequeños errores existentes en la aplicación.'
             ],
-            "deleted": [
+            'deleted': [
 
             ],
         },
         {
-            "version": "0.2.1",
-            "date": "2020/03/31",
-            "changed": [
-                "Mejoras a los mapas.",
+            'version': '0.2.1',
+            'date': '2020/03/31',
+            'changed': [
+                'Mejoras a los mapas.',
             ],
-            "added": [
-                "Botón para actualizar la información en la esquina superior derecha.",
-                "Enlace al bot de Telegram de Covid-19 Cuba Data en el menú lateral.",
+            'added': [
+                'Botón para actualizar la información en la esquina superior derecha.',
+                'Enlace al bot de Telegram de Covid-19 Cuba Data en el menú lateral.',
             ],
-            "fixed": [
+            'fixed': [
 
             ],
-            "deleted": [
-
-            ],
-        },
-        {
-            "version": "0.3.0",
-            "date": "2020/04/02",
-            "changed": [
-                "Gráfico de Cantidad de Test aplicados acumulados por días a Cantidad de Test aplicados por días.",
-            ],
-            "added": [
-                "Listado de las 10 provincias y 10 municipios con más casos diagnosticados.",
-                "Notificación para recordar el aplauso de las 9 de la noche dedicado quienes trabajan por la salud y seguridad del pueblo.",
-            ],
-            "fixed": [
-                "Errores en los mapas, como los colores de la escala.",
-                "Errores menores en la aplicación.",
-
-            ],
-            "deleted": [
+            'deleted': [
 
             ],
         },
         {
-            "version": "0.3.1",
-            "date": "2020/04/04",
-            "changed": [
-                "País por defecto para comparar con Cuba.",
+            'version': '0.3.0',
+            'date': '2020/04/02',
+            'changed': [
+                'Gráfico de Cantidad de Test aplicados acumulados por días a Cantidad de Test aplicados por días.',
             ],
-            "added": [
+            'added': [
+                'Listado de las 10 provincias y 10 municipios con más casos diagnosticados.',
+                'Notificación para recordar el aplauso de las 9 de la noche dedicado quienes trabajan por la salud y seguridad del pueblo.',
+            ],
+            'fixed': [
+                'Errores en los mapas, como los colores de la escala.',
+                'Errores menores en la aplicación.',
 
             ],
-            "fixed": [
-                "Errores en las tablas de los 10 provincias y 10 municipios con mayor cantidad de casos diagnosticados.",
-            ],
-            "deleted": [
-
-            ],
-        },
-        {
-            "version": "0.4.0",
-            "date": "2020/04/08",
-            "changed": [
-                "Comportamiento del país por defecto con que se compara Cuba. Ahora este será el último país con que se seleccionó para comparar.",
-                "Datos en algunas gráficas.",
-            ],
-            "added": [
-                "Información de los casos activos a la gráfica de Evolución de Casos por Días.",
-                "Gráfica de la Evolución de Altas por Días.",
-                "Gráfica de la Evolución de Muertes por Días.",
-                "Tabla con los 20 países con más casos acumulados.",
-                "Página de configuración accesible desde el menú lateral.",
-                "Posibilidad de escoger el modo de conexión entre: solo utilizar un servidor ubicado en Cuba, utilizar solo uno externo a Cuba o por último utilizar primero uno ubicado en Cuba y, si algo falla durante el proceso, utilizar el externo a Cuba.",
-            ],
-            "fixed": [
-                "Problema que impedía que las tablas de los municipios y provincias con más casos se mostrasen.",
-            ],
-            "deleted": [
+            'deleted': [
 
             ],
         },
         {
-            "version": "0.5.0",
-            "date": "2020/04/13",
-            "changed": [
-                "Añadidos todos los países en la comparación con Cuba.",
-                "Cambios en el menú lateral.",
+            'version': '0.3.1',
+            'date': '2020/04/04',
+            'changed': [
+                'País por defecto para comparar con Cuba.',
             ],
-            "added": [
-                "Estadísticas por provincia accediendo desde el menú lateral.",
-                "Gráfico para comparar los comportamientos exponenciales de la epidemia en los 20 países más afectados con Cuba."
-                "Pantalla para chequear si hay una actualización de la aplicación.",
-                "Notificaciones de actualización de la aplicación.",
-                "Notificaciones de actualización de la información.",
-                "Nuevo sistema de caché para disminuir la transferencia de datos por la red.",
-                "Animación al abrir y refrescar las pantallas.",
-            ],
-            "fixed": [
+            'added': [
 
             ],
-            "deleted": [
+            'fixed': [
+                'Errores en las tablas de los 10 provincias y 10 municipios con mayor cantidad de casos diagnosticados.',
+            ],
+            'deleted': [
+
+            ],
+        },
+        {
+            'version': '0.4.0',
+            'date': '2020/04/08',
+            'changed': [
+                'Comportamiento del país por defecto con que se compara Cuba. Ahora este será el último país con que se seleccionó para comparar.',
+                'Datos en algunas gráficas.',
+            ],
+            'added': [
+                'Información de los casos activos a la gráfica de Evolución de Casos por Días.',
+                'Gráfica de la Evolución de Altas por Días.',
+                'Gráfica de la Evolución de Muertes por Días.',
+                'Tabla con los 20 países con más casos acumulados.',
+                'Página de configuración accesible desde el menú lateral.',
+                'Posibilidad de escoger el modo de conexión entre: solo utilizar un servidor ubicado en Cuba, utilizar solo uno externo a Cuba o por último utilizar primero uno ubicado en Cuba y, si algo falla durante el proceso, utilizar el externo a Cuba.',
+            ],
+            'fixed': [
+                'Problema que impedía que las tablas de los municipios y provincias con más casos se mostrasen.',
+            ],
+            'deleted': [
+
+            ],
+        },
+        {
+            'version': '0.5.0',
+            'date': '2020/04/13',
+            'changed': [
+                'Añadidos todos los países en la comparación con Cuba.',
+                'Cambios en el menú lateral.',
+            ],
+            'added': [
+                'Estadísticas por provincia accediendo desde el menú lateral.',
+                'Gráfico para comparar los comportamientos exponenciales de la epidemia en los 20 países más afectados con Cuba.'
+                'Pantalla para chequear si hay una actualización de la aplicación.',
+                'Notificaciones de actualización de la aplicación.',
+                'Notificaciones de actualización de la información.',
+                'Nuevo sistema de caché para disminuir la transferencia de datos por la red.',
+                'Animación al abrir y refrescar las pantallas.',
+            ],
+            'fixed': [
+
+            ],
+            'deleted': [
+
+            ],
+        },
+        {
+            'version': '0.6.0',
+            'date': '2020/04/23',
+            'changed': [
+                'Comportamiento de las notificaciones de actualización de los datos.',
+            ],
+            'added': [
+                'Eventos de transmisión local activos',
+                'Eventos de transmisión local cerrados',
+                'Gráfica de la relación entre los tests (PCR) positivos respecto al total de tests (PCR) aplicados.',
+                'Gráfica de la evolución del Oxford Stringency Index para Cuba.',
+                'Cantidad de casos y tasa de diagnosticados por cada 100 mil habitantes en la tabla de provincias.',
+                'Cantidad de casos en la tabla de municipios.',
+                'Gráfica de comparación de los casos acumulados entre provincias.',
+                'Gráfica de comparación de los casos acumulados entre municipios.',
+                'Comparación de los casos activos, los diarios, los recuperados, los fallecidos y el Oxford Stringency Index de distintos países con Cuba.',
+                'Casos recuperados y fallecidos a la table de Top 20 de Países con más casos acumulados.',
+                'Pantalla de estadísticas por municipios, accesible desde el menú lateral.',
+                'Posibilidad de acceder a las estadíticas por provincias y municipios sin conexión para ver la última información que se descargó.',
+                'Pesquisador Virtual, accesible desde el menú lateral.',
+                'Tabla de Casos, accesible desde el menú lateral.',
+                'Respuesta a preguntas frecuentes, accesible desde el menú lateral.',
+                'Consejos y Respuestas sobre la Covid-19, accesible desde el menú lateral.',
+                'Pantalla con información del equipo de desarrollo, de colaboradores, de autores, etc. accesible desde el menú lateral.',
+            ],
+            'fixed': [
+                'Colapso del texto de los días en la gráfica de tests (PCR) por días.',
+            ],
+            'deleted': [
 
             ],
         },

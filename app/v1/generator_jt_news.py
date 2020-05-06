234dc67e8a6a522805d77a33517b56eb6bb0560c
leynier <leynier41@gmail.com>
2020-05-05 22:56:33 -0400

    Implement jt news

---
+++ app/v1/generator_jt_news.py
@@ -0,0 +1,24 @@
+from json import dump
+from feedparser import parse
+
+URL_JT_MEDIUM_FEED = 'https://medium.com/feed/juventud-t%C3%A9cnica/tagged/covid19'
+
+
+def generate(debug=False):
+    feed = parse(URL_JT_MEDIUM_FEED)
+    news = []
+    for entry in feed.entries:
+        news.append({
+            'id': entry['id'],
+            'link': entry['link'],
+            'title': entry['title'],
+            'author': entry['author'],
+            'published': entry['published_parsed'],
+            'updated': entry['updated_parsed'],
+            'summary': entry['summary'],
+        })
+    dump(news,
+        open(f'api/v1/jt_news.json', mode='w', encoding='utf-8'),
+        ensure_ascii=False,
+        indent=2 if debug else None,
+        separators=(',', ': ') if debug else (',', ':'))

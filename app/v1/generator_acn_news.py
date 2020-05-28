ea414136cfd391a06c2a5abe28a9abdf0ee738c3
Javier Oramas <javiale2000@gmail.com>
2020-05-28 19:22:22 -0400

    fixed data format dumped to json

--- app/v1/generator_acn_news.py
+++ app/v1/generator_acn_news.py
@@ -4,6 +4,7 @@ from json import dump
 import requests
 from bs4 import BeautifulSoup
 from os import path
+from feedparser import parse
 
 URL_ACN = 'http://www.acn.cu/busqueda?searchword=covid&ordering=newest&searchphrase=all&limit=0&areas[0]=categories&areas[1]=content&areas[2]=tags'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'}
@@ -18,7 +19,6 @@ def Extract_href(element):
     element = element[index+len('href="'):]
     element = element[:element.find('">')]    
     return element
-
 
 def remove_junk(string):
     new_str = ''
@@ -43,63 +43,55 @@ def remove_junk(string):
     for i in new_str:
         string=string+i
     return string 
+
 def clean_date(string):
     string = string[string.find('content="')+len('content="'):]
     return string[:string.find('"')]
 
-def clean_id(string):
-    string = string[string.find('value="')+len('value="'):]
-    return string[:string.find('"')]
 def generate(debug=False):
     news = []
     r = requests.get(URL_ACN,data = payload ,headers = headers)
-    
+
     soup = BeautifulSoup(r.text,'lxml')
     titles = soup.findAll('dt', {'class':'result-title'})
-    categories = soup.findAll('dd', {'class':'result-category'})
-    summaries = soup.findAll('dd', {'class':'result-text'})
+    abstracts = soup.findAll('dd', {'class':'result-text'})
     news_links = [Extract_href(str(i)) for i in titles]
+    
     for i,item in enumerate(news_links):
+        
         link ='http://www.acn.cu'+item
         r = requests.get(link,data = payload ,headers = headers)
+        
         soup = BeautifulSoup(r.text,'lxml')
         title = str(soup.find('h1', {'class':'article-title'}))
         author = str(soup.find('dd', {'class':'createdby hasTooltip'}))
         created = str(soup.find('meta', {'itemprop':'datePublished'}))
         updated = str(soup.find('meta', {'itemprop':'dateModified'}))
-        category = str(categories[i])
-        summary = str(summaries[i])
-       
-        id_check = soup.find('input', {'name':'object_id'})
-        if not id_check == None and not id_check == 'None':
-            new_id = str( id_check)
-        
-            news.append({
-                'id': clean_id(new_id),
-                'link': link,
-                'title': remove_junk(title),
-                'author': remove_junk(author),
-                'published': clean_date(created),
-                'updated': clean_date(updated),
-                'summary': remove_junk(summary),
-                'abstract': remove_junk(category)[1:-1],
-            })
+        abstract = str(abstracts[i])
+        summary = str(soup.find('section', {'class':'article-content'}))
+        news.append({
+            'id': link,
+            'link': link,
+            'title': remove_junk(title),
+            'author': remove_junk(author),
+            'published': clean_date(created),
+            'updated': clean_date(updated),
+            'summary': remove_junk(summary),
+            'abstract':remove_junk(abstract),
+        })
+    
     result = {
         'news': news,
     }
-    # # print(result)
-    # dump(result,
-    #     open(f'acn_news.json', mode='w', encoding='utf-8'),
-    #     ensure_ascii=False,
-    #     indent=2 if debug else None,
-    #     separators=(',', ': ') if debug else (',', ':'))
 
-    
     dump(result,
         open(f'api/v1/acn_news.json', mode='w', encoding='utf-8'),
+        # open(f'acn_news.json', mode='w', encoding='utf-8'),
+        
         ensure_ascii=False,
         indent=2 if debug else None,
         separators=(',', ': ') if debug else (',', ':'))
+
 
 
 def findnth(haystack, needle, n):

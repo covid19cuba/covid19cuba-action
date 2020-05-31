34e06c8b66b8445386d80306cc0f430bda45ade8
leynier <leynier41@gmail.com>
2020-05-31 07:03:09 -0400

    Add about_us, changelog and tips to api v2

--- app/v2/__init__.py
+++ app/v2/__init__.py
@@ -9,7 +9,7 @@ from .generator_provinces import generate as generate_provinces
 from ..static.about_us import about_us as data_about_us
 from ..static.app_version import APP_VERSION_CODE
 from ..static.changelog import changelog as data_changelog
-from ..static.tips import tips as advices
+from ..static.tips import tips as data_tips
 from ..utils import dump_util, send_msg
 
 
@@ -21,9 +21,9 @@ def run(debug=False):
         # generate_provinces(debug)
         # generate_municipalities(debug)
         generate_news(debug)
-        # build_changelog(debug)
-        # build_about_us(debug)
-        # build_tips(debug)
+        build_about_us(debug)
+        build_changelog(debug)
+        build_tips(debug)
         # build_full('api/v2', debug)
         # build_state(debug)
         if ok:
@@ -55,16 +55,28 @@ def state(data):
     return result
 
 
-def build_changelog(debug):
-    dump_util('api/v2', lambda _: data_changelog, debug=debug)
-
-
 def build_about_us(debug):
-    dump_util('api/v2', lambda _: data_about_us, debug=debug)
+    dump_util('api/v2', about_us, debug=debug)
+
+
+def about_us(_):
+    return data_about_us
+
+
+def build_changelog(debug):
+    dump_util('api/v2', changelog, debug=debug)
+
+
+def changelog(_):
+    return data_changelog
 
 
 def build_tips(debug):
-    dump_util('api/v2', lambda _: advices, debug= debug)
+    dump_util('api/v2', tips, debug= debug)
+
+
+def tips(_):
+    return data_tips
 
 
 def build_full(base_path, debug):

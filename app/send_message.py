d638f150b91b9ee5e725b75c078830989f4d2a5e
leynier <leynier41@gmail.com>
2020-05-31 06:12:22 -0400

    Add group id to env

--- app/send_message.py
+++ app/send_message.py
@@ -2,8 +2,7 @@ import os
 import telegram
 
 bot_token = os.getenv('INPUT_BOTTOKEN')
-# bot_token = '1122724836:AAF6K1XjbGfVykqoqUTARQA0Qa7A2ItEnZ8'
-group_id = -1001328230896
+group_id = int(os.getenv('INPUT_GROUPID'))
 
 def send(msg, chat_id=group_id, token=bot_token):
 	bot = telegram.Bot(token=token)

5ea5f65812fa4b85c0ed06562c6c8ad3947e88ce
Leynier Gutiérrez González <leynier41@gmail.com>
2020-06-10 04:15:50 -0400

    Update bulletins_generator.py

--- app/v2/extras_generator/bulletins_generator.py
+++ app/v2/extras_generator/bulletins_generator.py
@@ -2,34 +2,28 @@ from bs4 import BeautifulSoup
 from requests import get
 from urllib import request
 
-def bulletins(debug=False, base_url = 'https://covid19cubadata.github.io/'):
-    _bulletins = []
 
+def bulletins(debug = False, base_url = 'https://covid19cubadata.github.io/'):
+    result = []
     source = get(f'{base_url}boletines').text
-
     soup = BeautifulSoup(source,'lxml')
-
     for elem in soup.findAll('a'):
         description = elem.text
         link = elem.get('href')
         number = description[description.find('No.') + 4]
         size = int(request.urlopen(base_url + link).info()['Content-Length'])/10**6
-        _bulletins.append({
+        result.append({
             'id': number,
             'info': description,
             'url': link,
-            'size': size})
-
-    _bulletins.sort(key= lambda x : x['id'])
-
-    data = [
+            'size': size,
+        })
+    result.sort(key= lambda x : x['id'])
+    return [
         {
             'name': 'CEDEM',
             'url': 'http://www.biblioteca.uh.cu/red-bibliotecas/centro-estudios-demograficos-cedem',
             'data_source': base_url,
-            'bulletins': _bulletins
+            'bulletins': result
         }
     ]
-
-    return data
-    

4a4f928bba9d87fea531be15af0f52e58730d5b7
rmarticedeno <rmarticedeno@gmail.com>
2020-06-19 11:53:30 -0400

    Update Bulletins Json Structure

--- app/v2/extras_generator/bulletins_generator.py
+++ app/v2/extras_generator/bulletins_generator.py
@@ -16,14 +16,21 @@ def bulletins(debug = False, base_url = 'https://covid19cubadata.github.io/'):
             'id': number,
             'info': description,
             'url': link,
-            'size': size,
-        })
-    result.sort(key= lambda x : x['id'])
-    return [
-        {
-            'name': 'CEDEM',
-            'url': 'http://www.biblioteca.uh.cu/red-bibliotecas/centro-estudios-demograficos-cedem',
-            'data_source': base_url,
-            'bulletins': result
-        }
-    ]
+            'size': size})
+
+    _bulletins.sort(key= lambda x : x['id'])
+
+    data = { 
+        'providers': 
+        [ 
+            {
+                'name': 'CEDEM',
+                'url': 'http://www.biblioteca.uh.cu/red-bibliotecas/centro-estudios-demograficos-cedem',
+                'data_source': base_url,
+                'bulletins': _bulletins 
+            } 
+        ]
+    }
+
+    return data
+    

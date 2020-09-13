0c3f5e7f94c096bdc44ede25ac71dbeb826ead21
leynier <leynier41@gmail.com>
2020-09-12 22:33:31 -0400

    Set logs

--- app/v1/generator_jt_news.py
+++ app/v1/generator_jt_news.py
@@ -5,7 +5,9 @@ URL_JT_MEDIUM_FEED = 'https://medium.com/feed/juventud-t%C3%A9cnica/tagged/covid
 
 
 def generate(debug=False):
+    print("Parsing feed")
     feed = parse(URL_JT_MEDIUM_FEED)
+    print("Feed parsed")
     news = []
     for entry in feed.entries:
         summary = str(entry['summary'])
@@ -35,6 +37,6 @@ def generate(debug=False):
 
 def findnth(haystack, needle, n):
     parts= haystack.split(needle, n+1)
-    if len(parts)<=n+1:
+    if len(parts) <= n + 1:
         return -1
     return len(haystack) - len(parts[-1]) - len(needle)

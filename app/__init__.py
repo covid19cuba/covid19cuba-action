f65259d9fc9fa6a0643a25e9d66fbeed6541103b
leynier <leynier41@gmail.com>
2020-09-12 22:16:35 -0400

    Set logs to ap1 v1

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,11 @@ from .utils import send_msg
 
 
 def run(debug=False):
+    print('Running generator v1 ...')
     ok = v1run(debug)
+    print(f'Api v1 generated: {ok}')
+    print('Running generator v2 ...')
     ok = v2run(debug) and ok
+    print(f'Api v2 generated {ok}')
     if ok:
         send_msg('GitHub Action run successfully.', debug)
